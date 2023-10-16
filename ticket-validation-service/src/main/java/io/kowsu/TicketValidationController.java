7e25a01a772848fc8b2543a17268dcdcc6e60774
Ram Kowsu <ramanjineyulu.kld@gmail.com>
2023-10-16 06:53:52 +0530

    Adding config client changes to park project

--- ticket-validation-service/src/main/java/io/kowsu/TicketValidationController.java
+++ ticket-validation-service/src/main/java/io/kowsu/TicketValidationController.java
@@ -1,17 +1,24 @@
 package io.kowsu;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @RequestMapping("/status")
+@RefreshScope
 public class TicketValidationController {
+	
+	@Value("${custom.key}")
+	private String customValue;
 	
 	
 	@GetMapping("/health/check")
 	public String facilities() {
-		return "Ticket Validations are working.";
+		return "Ticket Validations are working." + customValue;
 	}
 
 }
--- ticket-validation-service/src/test/java/io/kowsu/TicketValidationServiceApplicationTests.java
+++ ticket-validation-service/src/test/java/io/kowsu/TicketValidationServiceApplicationTests.java
@@ -1,12 +1,10 @@
 package io.kowsu;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TicketValidationServiceApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
